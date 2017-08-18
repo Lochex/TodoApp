34ff64e78a30ad1208288c35ffe4cd8c977bef64
Andela Developer <andeladeveloper@Elo.local>
2017-08-18 11:50:30 +0100

    feat(ActionCreators): create login and signup ActionCreators [Finishes #150356274]

---
+++ client/app/src/actions/signupAction.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import toastr from 'toastr';
+import { SAVE_USER } from '../constants/actionTypes';
+import setAuthorizationToken from '../utils/setAuthorizationToken';
+
+const userSignupRequestSuccess = (user) => {
+  return {
+    type: SAVE_USER,
+    user
+  }
+}
+const userSignupRequest = (formDetails) => {
+  return dispatch => {
+    return axios.post('/api/v1/user', formDetails)
+    .then((response) => {
+      const { user, message } = response.data;
+
+      if (response.status === 201){
+         const token = response.data.jwt;
+         localStorage.setItem('jwtToken', token);
+         setAuthorizationToken(token);
+         dispatch(userSignupRequestSuccess(user));
+         toastr.success(message);
+      }
+    })
+    .catch(function (error) {
+      return error;
+    });
+  }
+}
+export { userSignupRequest };

0d5739a33f4107c441139612fb9eb4236630cc44
Andela Developer <andeladeveloper@Elo.local>
2017-08-25 14:54:30 +0100

    feat(Test): modify tests [Finishes #150356034]

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'development': 'mongodb://localhost/TodoApp',
-  'test': 'mongodb://localhost/TodoTest',
+  'development': 'mongodb://elo:dtabitha123@ds025752.mlab.com:25752/todoappandela',
+  'test': 'mongodb://elo:dtabitha123@ds159033.mlab.com:59033/andelatodotest',
 };
 
